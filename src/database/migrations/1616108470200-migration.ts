103b5fe5c89d00031cb23e74d42afed62cc4e463
Stephany <tete.1818@hotmail.com>
2022-01-13 09:27:29 -0300

    adding new tables

--- src/database/migrations/1616108470200-migration.ts
+++ src/database/migrations/1616108470200-migration.ts
@@ -25,9 +25,64 @@ export class migration1616108470200 implements MigrationInterface {
     await queryRunner.query(
       'ALTER TABLE "users_games_games" ADD CONSTRAINT "FK_934b0d8f9d0084c97d3876ad32d" FOREIGN KEY ("gamesId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE NO ACTION',
     );
+
+    await queryRunner.query(
+      'CREATE TABLE "genres" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), PRIMARY KEY ("id"))'
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE "games_genres" ("gameId" uuid NOT null, "genresId" uuid NOT null, PRIMARY KEY ("gameId", "genresId"))'
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE "games_genres" ADD CONSTRAINT "FK_id_games" FOREIGN KEY ("gameId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE NO action'
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE "games_genres" ADD CONSTRAINT "FK_id_genres" FOREIGN KEY ("genresId") REFERENCES "genres"("id") ON DELETE CASCADE ON UPDATE NO action'
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE "orders" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" uuid NOT null, PRIMARY KEY ("id"))'
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE "orders" ADD CONSTRAINT "FK_id_users" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO action'
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE "orders_games" ("ordersId" uuid NOT null, "gamesId" uuid NOT null, PRIMARY KEY ("ordersId", "gamesId"))'
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE "orders_games" ADD CONSTRAINT "FK_id_games" FOREIGN KEY ("gamesId") REFERENCES "games"("id") ON DELETE CASCADE ON UPDATE NO action'
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE "orders_games" ADD CONSTRAINT "FK_id_orders" FOREIGN KEY ("ordersId") REFERENCES "orders"("id") ON DELETE CASCADE ON UPDATE NO action'
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE "orders_games" DROP CONSTRAINT  "FK_id_orders"'
+    );
+    await queryRunner.query(
+      'ALTER TABLE "orders_games" DROP CONSTRAINT  "FK_id_games"'
+    );
+    await queryRunner.query('DROP TABLE "orders_games"');
+    await queryRunner.query(
+      'ALTER TABLE "orders" DROP CONSTRAINT  "FK_id_users"'
+    );
+    await queryRunner.query('DROP TABLE "orders"');
+    await queryRunner.query(
+      'ALTER TABLE "games_genres" DROP CONSTRAINT  "FK_id_genres"'
+    );
+    await queryRunner.query(
+      'ALTER TABLE "games_genres" DROP CONSTRAINT  "FK_id_games"'
+    );
+    await queryRunner.query('DROP TABLE "games_genres"');
+    await queryRunner.query('DROP TABLE "genres"');
     await queryRunner.query(
       'ALTER TABLE "users_games_games" DROP CONSTRAINT "FK_934b0d8f9d0084c97d3876ad32d"',
     );
